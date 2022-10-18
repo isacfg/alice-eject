83e945cb5ef91599ea4ccad5ad00313ee5ca8b84
isacfg <isaac.lima.105@ufrn.edu.br>
2022-10-18 18:22:41 -0300

    #1.0v
    - Animations
    - Add labels Form
    - Bigger hamburguer button
    - Copy

---
+++ js/animations.js
@@ -0,0 +1,35 @@
+
+// Nav
+ScrollReveal().reveal('.navbar', {
+    delay: 0,
+    duration: 3000,
+    distance: '-50px',
+})
+
+// Home
+ScrollReveal().reveal('.reveal-headline', {
+    delay: 0,
+    duration: 3000,
+    distance: '-50px',
+})
+ScrollReveal().reveal('.reveal-subheadline', {
+    delay: 500,
+    duration: 3000,
+    distance: '-50px',
+})
+ScrollReveal().reveal('.reveal-btn', {
+    delay: 1500,
+    duration: 3000,
+    // distance: '50px',
+})
+
+// Portfolio
+ScrollReveal().reveal('.portfolio-item', {
+    delay: 0,
+    scale: 0.5,
+    duration: 500,
+    origin: 'bottom',
+    distance: '1000px',
+    easing: 'ease-in-out',
+    interval: 150,
+})
