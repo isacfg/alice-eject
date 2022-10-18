947c656897ea19a4173ffaa7bc5156f7d6c78a90
isacfg <isaac.lima.105@ufrn.edu.br>
2022-10-18 15:55:51 -0300

    #0.5v
    #0.5v
    - Compress images
    - Remove original images folder
    - Slider Parceiros

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,42 @@
 $(document).ready(function () {
-    $('.slider').slick({
-        setting-name: setting-value
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
+
+
+
